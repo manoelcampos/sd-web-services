82e7de988ae2e2ed36b30ec242dc843930a42882
Manoel Campos <manoelcampos@gmail.com>
2020-10-06 14:39:20 -0300

    Adiciona config pra carregar import.sql em prod

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,16 @@
 insert into Cidade (id, nome, uf) values (1, 'Palmas', 'TO');
-insert into Cidade (id, nome, uf) values (2, 'Paraiso', 'TO');
-insert into Cidade (id, nome, uf) values (3, 'Porto Nacional', 'TO');
+insert into Cidade (id, nome, uf) values (2, 'Araguaina', 'TO');
+insert into Cidade (id, nome, uf) values (3, 'Gurupi', 'TO');
+insert into Cidade (id, nome, uf) values (4, 'Porto Nacional', 'TO');
 
-insert into Cliente (id, dataCadastro, nome) values (1, current_date(), 'Manoel');
-insert into Cliente (id, dataCadastro, nome) values (2, current_date(), 'Raysa');
-insert into Cliente (id, dataCadastro, nome) values (3, current_date(), 'Breno');
+insert into Usuario (id, cpf, nome, cidade_id) values (1, '11111111111', 'Manoel', 1);
+insert into Usuario (id, cpf, nome, cidade_id) values (2, '22222222222', 'Maria', 1);
+insert into Usuario (id, cpf, nome, cidade_id) values (3, '33333333333', 'Raimundo', 2);
 
-insert into Endereco (id, logradouro, cidade_id, cliente_id) values (1, '310 Sul', 1, 1);
+insert into Cliente (id, nome) values (1, 'Jose');
+insert into Cliente (id, nome) values (2, 'Pedro');
+insert into Cliente (id, nome) values (3, 'Rosana');
 
-insert into Usuario (id, cpf, nome, cidade_id) values (1, '11111111111', 'Maria', 1);
-insert into Usuario (id, cpf, nome, cidade_id) values (2, '22222222222', 'Joao', 2);
-insert into Usuario (id, cpf, nome, cidade_id) values (3, '33333333333', 'Ana', 1);
-insert into Usuario (id, cpf, nome, cidade_id) values (4, '44444444444', 'Helder', 3);
+insert into Endereco (id, logradouro, cidade_id, cliente_id) values (1, 'Rua 1', 1, 1);
+insert into Endereco (id, logradouro, cidade_id, cliente_id) values (2, 'Rua 1', 1, 2);
+insert into Endereco (id, logradouro, cidade_id, cliente_id) values (3, 'Rua 2', 2, 3);
